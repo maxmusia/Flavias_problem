6f16d68acdfc1ad45b7aef9ff51aa86a54ae8f9d
maxmusia <maxmusia@gmail.com>
2024-09-12 20:23:35 +0300

    Simle miner without OOP

--- main.py
+++ main.py
@@ -1,9 +1,2 @@
-
-n, k = int(input()), int(input())
-l = []
-for i in range(1, n+1):
-    l+=[i]
-while len(l) != 1:
-    l+= l[:(k-1)]
-    del(l[:(k)])
-    print(*l)
+for i in range(10):
+    print('Hello', end=' ')
